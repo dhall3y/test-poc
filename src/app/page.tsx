859897c67b83aeb4997b1d7a157c031bd108b44e
dhall3y <dhalley@null.net>
2024-07-09 15:41:49 +0200

    added crypto-js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import styles from './styles/Home.module.css';
 
 const HomePage: React.FC = () => {
   const videoUrls = [
-    'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+    //https://{pull_zone_url}.b-cdn.net/{video_id}/play_{resolution_height}p.mp4
+    'https://vz-b8553144-3b2.b-cdn.net/98394773-31f7-40c5-8199-232417fd92d0/play_720p.mp4'
   ];
 
   return (
